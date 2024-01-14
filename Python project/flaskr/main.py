7b8e0ab19ef82b8c5d2e01a2cdddd2482f366b53
sENDMEmess <a.stawska02@gmail.com>
2024-01-14 20:04:24 +0100

    version 2

--- Python project/flaskr/main.py
+++ Python project/flaskr/main.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, url_for
 from flaskr import app
 import sqlite3
+import random
+import string
 DATABASE = 'database.db'
 
 
@@ -12,7 +14,12 @@ def index():
 
     books = []
     for row in db_books:
-        books.append({'title': row[0],'price':row[1], 'arrival_day':row[2]})
+        books.append({
+            'title': row[0],
+            'price': row[1],
+            'arrival_day': row[2],
+            'color': row[3]
+        })
 
     return render_template(
         'index.html',
@@ -23,15 +30,21 @@ def index():
 def form():
     return render_template('form.html')
 
+def generate_color():
+    return '#'+ ''.join(random.choice(string.hexdigits) for _ in range(6))
+
 @app.route('/register', methods=['POST'])
 def register():
     title=request.form['title']
     price=request.form['price']
     arrival_day=request.form['arrival_day']
+    color = generate_color()
+
 
     con=sqlite3.connect(DATABASE)
-    con.execute('INSERT INTO books VALUES(?,?,?)',
-                [title, price, arrival_day])
+    con.execute('INSERT INTO books VALUES(?,?,?,?)',
+                [title, price, arrival_day, color])
     con.commit()
     con.close()
     return redirect(url_for('index'))
+
